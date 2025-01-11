51c25e0e09a6dbc877242f8b4e27ea83bb6e4274
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-01-10 21:24:16 -0300

    CriadoClasseSeller

--- src/application/ProgramJDBC.java
+++ src/application/ProgramJDBC.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class ProgramJDBC {
 
@@ -8,6 +11,11 @@ public class ProgramJDBC {
 		
 		Departament obj = new Departament(1, "Books");
 		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
+		
 	}
 
 }
