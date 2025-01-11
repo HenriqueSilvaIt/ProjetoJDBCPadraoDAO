3225547bb3707d9460a98c727215e86a9fb246fe
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-01-11 06:32:46 -0300

    SellerDaoJDBC(ClasseImplementacao)DaoFactory(ClasseInstanciacao)

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,17 @@
 package model.dao;
 
-public interface DepartmentDao {
+import java.util.List;
 
+import model.entities.Departament;
+
+public interface DepartmentDao {
+	
+	void insert(Departament obj); // responsavel por inserir no banco
+	void update(Departament obj); // update no banco
+	void deleteById(Integer id); // delete no banco pelo ID
+	Departament findById (Integer id); // Procura o departamento pelo ID
+	List<Departament> findall(); // retorna todos os departamento por isso tem que ser
+	// lista
+	
+	
 }
